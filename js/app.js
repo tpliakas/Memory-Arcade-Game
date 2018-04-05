8eb3c27d235c88e056d7eecfbfa3a9337fef64c3
Thodoris Pliakas <thodorispliakas@gmail.com>
2018-04-05 22:44:43 +0300

    feat: Add function to shufle the array

--- js/app.js
+++ js/app.js
@@ -40,6 +40,22 @@ function displayNewCards() {
   document.querySelector(".deck").addEventListener('click', cardClick)
 }
 
+/******  Shuffle array  *******/
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 
 // const visualTimer = document.getElementById('visual-timer');
 // let seconds = 0;
